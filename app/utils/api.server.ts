fe9b5020a3255ea8dcfd16c5dbfa25ea792ea6d1
skyclo <hello.skyclo@gmail.com>
2025-10-26 04:32:03 -0400

    👽️ update endpoints, add wip summary endpoint

--- app/utils/api.server.ts
+++ app/utils/api.server.ts
@@ -5,7 +5,7 @@ export async function gen(args: any, questions: any, env: any) {
 
     const q = questions || []
 
-    const res = await fetch(url, {
+    const res = await fetch(url + "/gen_personas", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -15,6 +15,29 @@ export async function gen(args: any, questions: any, env: any) {
             age: { low: args.age?.low || 18, high: args.age?.high || 60 },
             questions: q,
         }),
+    })
+
+    try {
+        if (res.status == 404) {
+            return { error: "API endpoint not started" }
+        } else if (!res.ok) {
+            throw new Error(await res.text())
+        }
+        return await res.json()
+    } catch (e) {
+        console.error("Error parsing API response JSON:", e)
+        return Promise.reject({ error: "Invalid JSON response from API. Status: " + res.status })
+    }
+}
+
+export async function summary(responses: any[], env: any) {
+    const url = env?.N8N_WEBHOOK_URL
+    if (!url) throw new Error("API URL not specified.")
+
+    const res = await fetch(url + "/gen_summary", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ responses }),
     })
 
     try {
